6ccb99c9719685c70b6ca36c67b928c34009a07a
yami-beta <yami-beta@users.noreply.github.com>
2019-06-17 16:57:43 +0900

    Revert "fix: Notify only user 1"
    This reverts commit d2000dd3e62dbc94bbf14647ceb4e9d0fb2566f5.

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,8 +2,7 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    # ActionCable.server.broadcast 'room_channel', message: render_message(message)
-    RoomChannel.broadcast_to("1", message: render_message(message))
+    ActionCable.server.broadcast 'room_channel', message: render_message(message)
   end
 
   private
