fc83fb9aa55b98bdb26c6cc0294e4c4219b71e8b
yami-beta <yami-beta@users.noreply.github.com>
2019-06-17 15:28:52 +0900

    feat: Implement login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
   resources :users
+  resources :user_sessions
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
   get 'rooms/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
